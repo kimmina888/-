db00b6a51f78ebe17967985ae8bb5494c19db7dd
mina kim <81088157+kimmina888@users.noreply.github.com>
2022-04-22 00:18:00 +0900

    second commit

--- src/one.js
+++ src/one.js
@@ -1,12 +1,19 @@
 "use strict";
-let security_key ="coffee";
-let s = new String("i want to drink coffee");
-let result = new Array();
-let Alp = new Array("abcdefghijklmnopqrstuvwxyz");
-s.trim();
-let sk = new String(new Set(security_key.trim()+Alp));
-
-for(let i = 0; i < s.length; i++){
-    result.push(sk.charAt(Alp.indexOf(s.charAt(i))));
+function one(security_key, s){
+    let result = new String();
+    console("오류");
+    let Alp = new String("abcdefghijklmnopqrstuvwxyz");
+    console("오류");
+    s.trim();
+    console("오류");
+    let sk = new String(new Set(security_key.trim()+Alp));
+    
+    for(let i = 0; i < s.length; i++){
+        console("오류");
+        result += sk.charAt(Alp.indexOf(s.charAt(i)));
+    }
+    console("오류");
+    document.body.getElementById('pwsText') = result;
+    console("오류");
 }
-console.log(result);
+
