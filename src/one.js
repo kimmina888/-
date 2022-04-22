f5cce8bbd754fe10adfd8e55ffb9277269e408da
mina kim <81088157+kimmina888@users.noreply.github.com>
2022-04-22 10:34:30 +0900

    error fix

--- src/one.js
+++ src/one.js
@@ -1,15 +1,15 @@
-"use strict";
-function one(security_key, s){
+"use strict";s
+function one(security_key, ){
     let result = "";
     let Alp = new String("abcdefghijklmnopqrstuvwxyz");
     s.trim();
-    let sk = (security_key.trim())+Alp;
+    let sk = new String((security_key.trim())+Alp);
     sk = (new Set(sk));
     console.log(sk);
 
     for(let i = 0; i < s.length; i++){
         // console.log("오류");
-        result += Alp.indexOf(s.charAt(i));
+        result += sk.charAt(Alp.indexOf(s.charAt(i)));
     }
 
     console.log("오류");
--- src/two.js
+++ src/two.js
@@ -1,41 +1,70 @@
-function two(security_key, s){
-    let result = "";
-    let Alp = new Array(Array(), Array(), Array(), Array(), Array());
-    security_key.trim();
-    let securityKey = new Set(security_key);
-    let sString = s.trim();
-    console.log(sk);
-    let z = 0;
-    for(let i = 0; i < 5; i++){
-        for(let j =0; k < 5; j++){
-            if(Alp.includes(securityKey[z])){
+function two(security_key, s) {
+    let alphabetBoard = new Array(Array(), Array(), Array(), Array(), Array());
+    let oddFlag = false;
+    let playFair = new Array();
+    let encPlayFair = new Array();
+    let x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+    let encStr = "";
 
-            }else{
-                Alp.push(securityKey[z]);
+    for (let i = 0; i < s.length; i += 2) {
+        let tmpArr = new Array(2);
+        tmpArr[0] = s.charAt(i);
+        try {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                tmpArr[1] = 'x';
+                i--;
+            } else {
+                tmpArr[1] = s.charAt(i + 1);
             }
+        } catch (error) {
+            tmpArr[1] = 'x';
+            oddFlag = true;
+        }
+        playFair.push(tmpArr);
+    }
+    for (let i = 0; i < playFair.length; i++) {
 
-        }
-    }
-    for(let c = 'a'; c <= 'z'; c++){
-        if(Alp.includes(c)){
-
-        }else{
-            Alp.push(c);
-        }
-    }
-    for(let i =0; i < sString.length - 1; i++){
-        if(sString[i] == sString[i+1]){
-            sString[i+1] = 'x';
-        }
-    }
-    if(sString.length % 2 == 1){
-        sString.push('x');
-    }
-    for(let i =0; i < sString.length; i+=2){
-        for(let x = 0; x < 5; x++){
-            for(let y = 0; y < 5; y++){
-               
+        let tmpArr = new Array(2);
+        for (let j = 0; j < alphabetBoard.length; j++) //쌍자암호의 각각 위치체크
+        {
+            for (let k = 0; k < alphabetBoard[j].length; k++) {
+                if (alphabetBoard[j][k] == playFair.get(i)[0]) {
+                    x1 = j;
+                    y1 = k;
+                }
+                if (alphabetBoard[j][k] == playFair.get(i)[1]) {
+                    x2 = j;
+                    y2 = k;
+                }
             }
         }
+
+
+        if (x1 == x2) //행이 같은경우
+        {
+            tmpArr[0] = alphabetBoard[x1][(y1 + 1) % 5];
+            tmpArr[1] = alphabetBoard[x2][(y2 + 1) % 5];
+        }
+        else if (y1 == y2) //열이 같은 경우
+        {
+            tmpArr[0] = alphabetBoard[(x1 + 1) % 5][y1];
+            tmpArr[1] = alphabetBoard[(x2 + 1) % 5][y2];
+        }
+        else //행, 열 모두 다른경우
+        {
+            tmpArr[0] = alphabetBoard[x2][y1];
+            tmpArr[1] = alphabetBoard[x1][y2];
+        }
+
+        encPlayFair.push(tmpArr);
+
     }
+
+    for (let i = 0; i < encPlayFair.length; i++) {
+        encStr += encPlayFair[i][0] + "" + encPlayFair[i][1] + " ";
+    }
+
+
+    console.log(encStr);
 }
+two("coffee", "i want to drink coffee");
